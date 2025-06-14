247968b08cc403409f7e47f80fb25c057c46ace9
Chelsigupta14 <guptachelsi1409@gmail.com>
2025-06-14 22:33:04 +0530

    Updated Navbar sections

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,19 +1,29 @@
 // src/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa'; // icons for menu
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
       <div className="logo">Chelsi</div>
-      <div className="nav-links">
-        <Link to="about" smooth={true} duration={500}>About</Link>
-        <Link to="skills" smooth={true} duration={500}>Skills</Link>
-        <Link to="projects" smooth={true} duration={500}>Projects</Link>
-        <Link to="contact" smooth={true} duration={500}>Contact</Link>
-        <Link to="certifications" smooth={true} duration={500}>Certifications</Link>
 
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <Link to="about" smooth={true} duration={500} onClick={closeMenu}>About</Link>
+        <Link to="skills" smooth={true} duration={500} onClick={closeMenu}>Skills</Link>
+        <Link to="projects" smooth={true} duration={500} onClick={closeMenu}>Projects</Link>
+        <Link to="certifications" smooth={true} duration={500} onClick={closeMenu}>Certifications</Link>
+        <Link to="contact" smooth={true} duration={500} onClick={closeMenu}>Contact</Link>
+      </div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
       </div>
     </nav>
   );
